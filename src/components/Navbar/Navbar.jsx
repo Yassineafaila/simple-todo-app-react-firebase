bcdbc1266c3bae47104d760c723fdc8acd784009
Yassineafaila <yassineafaila55@gmail.com>
2023-10-24 20:41:38 +0100

    style:add the styles and also the responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useAuth } from '../../context/AuthContext'
-import Button from '../Ui/button'
-import {AiOutlineMenu} from "react-icons/ai"
+import React, { useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import Button from "../Ui/button";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 function Navbar() {
-   const {isAuthenticated}=useAuth()
+  const [showMenu, setShowMenu] = useState(false);
+  const { isAuthenticated } = useAuth();
   return (
-    <nav className="navbar navbar-expand-lg position-relative">
-      <div className="container">
-        <div>
-          <Link className="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg position-relative ">
+      <div className="container pb-3">
+        <div className="brand-container">
+          <Link className="navbar-brand fw-bold" to="/">
             Todo Task
           </Link>
-          <Button>
-            <AiOutlineMenu/>
-          </Button>
+          <div className="d-md-none d-lg-none d-xl-none">
+            {!showMenu ? (
+              <Button HandlerClick={() => setShowMenu(!showMenu)}>
+                <AiOutlineMenu size={25} />
+              </Button>
+            ) : (
+              <Button HandlerClick={() => setShowMenu(!showMenu)}>
+                <AiOutlineClose size={25} />
+              </Button>
+            )}
+          </div>
         </div>
-        <div className="navbar-container">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
+        <div className={`navbar-container  ${showMenu ? "show" : ""}`}>
+          <ul className="navbar-nav flex-md-row gap-2 gap-md-5 gap-lg-4 gap-xl-4 ">
+            <li className="nav-item  py-1 px-3">
+              <Link className="nav-link fw-medium" to="/">
                 Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/todo">
+            <li className="nav-item py-1 px-3">
+              <Link className="nav-link fw-medium" to="/todo">
                 Todo
               </Link>
             </li>
             {isAuthenticated ? (
-              <li className="nav-item">
-                <Link className="nav-link" to="/profile">
+              <li className="nav-item py-1 px-3">
+                <Link className="nav-link fw-medium" to="/profile">
                   Profile
                 </Link>
               </li>
             ) : (
-              <li className="nav-item">
-                <Link className="nav-link" to="/sign-in">
+              <li className="nav-item py-1 px-3">
+                <Link className="nav-link fw-medium" to="/sign-in">
                   Log In
                 </Link>
               </li>
@@ -48,4 +57,4 @@ function Navbar() {
   );
 }
 
-export default Navbar
+export default Navbar;
