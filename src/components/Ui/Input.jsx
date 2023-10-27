3d801ccc22b0a60c2cbb8e3bc4eab1bd05aafe76
Yassineafaila <yassineafaila55@gmail.com>
2023-10-27 13:01:46 +0100

    feat: add new feature to the input which is label

--- src/components/Ui/Input.jsx
+++ src/components/Ui/Input.jsx
@@ -1,18 +1,38 @@
 import React from "react";
-
-function Input({ type, placeholder, value, HandlerChange, name, className,error }) {
+import "./Input.css";
+function Input({
+  type,
+  placeholder,
+  value,
+  HandlerChange,
+  name,
+  className,
+  error,
+  children,
+  label
+}) {
   return (
-    <div>
-      <input
-        type={type}
-        name={name}
-        className={className}
-        placeholder={placeholder}
-        value={value}
-        onChange={HandlerChange}
-      ></input>
-      {error && <p>{error}</p>}
-    </div>
+    <>
+      <div className="input-container d-flex flex-column gap-1 ">
+        {label && <label>{label}</label>}
+        <div
+          className={`d-flex border rounded py-2 px-3 border-light-subtle w-100 align-items-center gap-2 ${
+            error ? "error" : ""
+          }`}
+        >
+          {children}
+          <input
+            type={type}
+            name={name}
+            className={className}
+            placeholder={placeholder}
+            value={value}
+            onChange={HandlerChange}
+          ></input>
+        </div>
+        {error && <p className={`${error ? "error" : ""}`}>{error}</p>}
+      </div>
+    </>
   );
 }
 
