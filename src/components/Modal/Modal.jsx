d480f387de5b696ed384097577f58599673e5c2c
Yassineafaila <yassineafaila55@gmail.com>
2023-10-27 12:59:45 +0100

    refactor: adding new condition to render the modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,39 @@
-import React from "react";
-import  ReactDom  from "react-dom";
+import React, { useState, useEffect } from "react";
+import ReactDom from "react-dom";
 import "./Modal.css";
-function Modal({ children }) {
-  return ReactDom.createPortal(
-    <>
-      <div className="overlay"></div>
-      <div className="modal-box">
-        {children}
-      </div>
-    </>
-  ,document.getElementById("portal"));
+import Button from "../Ui/button";
+import { AiOutlineClose } from "react-icons/ai";
+function Modal({ children, message }) {
+  const [show, setShow] = useState(false);
+  console.log(message)
+  useEffect(() => {
+    if (message) {
+      const time = setTimeout(() => {
+        setShow(true);
+      });
+      return () => {
+        clearTimeout(time);
+        setShow(false);
+      };
+    }
+  }, [message]);
+
+  if (show) {
+    return ReactDom.createPortal(
+      <>
+        <div className="overlay"></div>
+        <div className="modal-box py-2 px-3 d-flex align-items-center justify-content-center">
+          {children}
+          <Button HandlerClick={() => setShow(false)} className={"border-0 bg-dark text-white rounded"}>
+            <AiOutlineClose />
+          </Button>
+        </div>
+      </>,
+      document.getElementById("portal")
+    );
+  }
+
+  return null;
 }
 
 export default Modal;
